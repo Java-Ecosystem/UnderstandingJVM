f22032111a873e2e002a23b32328fefbcafb10ee
zhangliangbo <2472110501@qq.com>
2017-07-18 08:49:55 +0800

    引用计数垃圾回收法

---
+++ Code/Code/src/main/java/jvm/three/one/ReferenceCountingGC.java
@@ -0,0 +1,26 @@
+package jvm.three.one;
+/**
+ * vm args：-verbose:gc -XX:+PrintGCDetails
+ * @author zlb
+ *
+ */
+public class ReferenceCountingGC {
+
+	public Object instance = null;
+
+	private static final int _1MB = 1024 * 1024;
+
+	private byte[] bigSize = new byte[2 * _1MB];
+
+	public static void main(String[] args) {
+		ReferenceCountingGC objA = new ReferenceCountingGC();
+		ReferenceCountingGC objB = new ReferenceCountingGC();
+		objA.instance = objB;
+		objB.instance = objA;
+		objA = null;
+		objB = null;
+		//
+		System.gc();
+	}
+
+}
